dea529655070d21567145a0e14157eab50fa90cd
Jerojose <132441170+Jerojose@users.noreply.github.com>
2023-05-03 12:43:32 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+let variable2 = 0
+let variable1 = 0
+input.onButtonPressed(Button.A, function () {
+    variable2 = 2
+    basic.showNumber(variable2)
+    variable1 = randint(0, 10)
+    basic.showNumber(variable1)
+    basic.showNumber(variable1 * variable2)
+})
+input.onButtonPressed(Button.B, function () {
+    variable2 = 3
+    basic.showNumber(variable2)
+    variable1 = randint(0, 10)
+    basic.showNumber(variable1)
+    basic.showNumber(variable1 * variable2)
+})
